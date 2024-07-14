bff0d098cfd9577070316931beb2eaf114bb2a19
Lavender <anyangolavenderr@gmail.com>
2024-07-14 10:49:11 +0300

    center button

--- taishi/src/Podcast/index.js
+++ taishi/src/Podcast/index.js
@@ -1,34 +1,30 @@
 import podcast from '../Assets/podcast.png';
-import microphone from '../Assets/microphone.jpg'
+import microphone from '../Assets/microphone.jpg';
 import './index.css';
 // import Navbar from "../Navbar";
 
-const Podcast = function (){
-    return(
-       <>
-       {/* <Navbar/> */}
-        <section className="banner" style={{ backgroundImage: `url(${podcast})` }}>
+const Podcast = function () {
+  return (
+    <>
+      {/* <Navbar /> */}
+      <section className="banner" style={{ backgroundImage: `url(${podcast})` }}>
         <div className="image-container">
           <div className="overlay">
             <div className="content">
               <h3 className="body-text">Taishi Podcasts</h3>
               <div className="coming-soon">
-              <img src={microphone} alt="Coming Soon" />
-                
-               <div>
-               <h3 className="body-text">Coming Soon</h3>
-               <button className="get-started-btn">All Episodes</button>
-
-               </div>
+                <img src={microphone} alt="Coming Soon" />
+                <div>
+                  <h3 className="body-text">Coming Soon</h3>
+                  <button className="get-started-btn">All Episodes</button>
+                </div>
               </div>
-            
-              
             </div>
           </div>
         </div>
       </section>
-       </>
-    )
+    </>
+  );
 }
 
 export default Podcast;
