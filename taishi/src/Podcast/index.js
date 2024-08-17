b4173d7d83bf77fb7a9ec220bfe4419ce8d28875
Lavender <anyangolavenderr@gmail.com>
2024-08-17 11:25:35 +0300

    podacasts

--- taishi/src/Podcast/index.js
+++ taishi/src/Podcast/index.js
@@ -1,12 +1,12 @@
 import podcast from '../Assets/podcast.png';
 import microphone from '../Assets/microphone.jpg';
 import './index.css';
-// import Navbar from "../Navbar";
+import podcastAudio from '../Assets/UnchainedHeartsEP1.mp3';
+
 
 const Podcast = function () {
   return (
     <>
-      {/* <Navbar /> */}
       <section className="banner" style={{ backgroundImage: `url(${podcast})` }}>
         <div className="image-container">
           <div className="overlay">
@@ -15,7 +15,15 @@ const Podcast = function () {
               <div className="coming-soon">
                 <img src={microphone} alt="Coming Soon" />
                 <div>
-                  <h3 className="body-text">Coming Soon</h3>
+                <div className="podcast-container">
+      <div className="podcast-player">
+      
+        <audio controls>
+          <source src={podcastAudio} type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
+      </div>
+    </div>
                   <button className="get-started-btn">All Episodes</button>
                 </div>
               </div>
